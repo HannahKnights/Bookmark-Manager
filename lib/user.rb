9745a5f8f13a65d494d66cc43119e5ae0bfc1b9f
Hannah Knights <hannah.knights@hotmail.co.uk>
2013-12-18 09:25:10 +0000

    Password form hides password

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true
   property :password_digest , Text
 
   validates_confirmation_of :password
