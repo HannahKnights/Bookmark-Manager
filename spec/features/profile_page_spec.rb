1e22016d39ca905af6daf9fb7149b6a0b51a21af
Hannah Knights <hannahknights@hotmail.co.uk>
2013-12-31 13:15:33 +0000

    Modifications to a profile page

---
+++ spec/features/profile_page_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+feature "User's profile page" do 
+
+  scenario "displays the tags a user has submitted" do
+    sign_up
+    sign_in('test@test.com', 'test')
+    add_link("http://makersacademy.com",
+            "Makers Academy",
+            ['education'])
+    click_link "My Favourites"
+    expect(page).to have_content("My Tags: education")
+  end
+
+  scenario "displays the links that they've submitted" do
+    sign_up
+    sign_in('test@test.com', 'test')
+    add_link("http://makersacademy.com",
+            "Makers Academy",
+            ['education'])
+    add_link("http://test.com",
+            "Test",
+            ['test'])
+    click_link "My Favourites"
+    expect(page).to have_content("My Links: Makers Academy Test")
+  end
+  
+end
