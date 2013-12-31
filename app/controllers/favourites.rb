de28ca90cd7ef1ef78f6ff2a06061a67124e39c4
Hannah Knights <hannahknights@hotmail.co.uk>
2013-12-31 12:31:09 +0000

    Added a one to many relationship between user and link. User can only add a link if signed in

--- app/controllers/favourites.rb
+++ app/controllers/favourites.rb
@@ -1,7 +1,13 @@
 post '/favourites' do
-  link_id, user_id = params['favourite_link'], session[:user_id]
-  if LinkUser.first(:user_id => user_id, :link_id => link_id) == nil
-    LinkUser.create(:user_id => user_id, :link_id => link_id)
+  link_id = params['favourite_link']
+  link_user_id = Link.first(:id => link_id).user_id 
+  user_id = session[:user_id]
+  if LinkUser.first(:user_id => user_id,
+                   :link_id => link_id,
+                   :link_user_id => link_user_id) == nil
+    LinkUser.create(:user_id => user_id,
+                   :link_id => link_id,
+                   :link_user_id => link_user_id)
   else
   end
   redirect to '/'
