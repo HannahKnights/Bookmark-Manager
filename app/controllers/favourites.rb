1a228d268fc39a95b98af8c7dd7d8994a9d14df1
Hannah Knights <hannahknights@hotmail.co.uk>
2014-01-03 19:46:25 +0000

    Refactoring on the controllers

--- app/controllers/favourites.rb
+++ app/controllers/favourites.rb
@@ -5,9 +5,10 @@ post '/favourites' do
                    :link_id => link_id,
                    :link_user_id => link_user_id) == nil
     LinkUser.create(:user_id => user_id,
-                   :link_id => link_id,
-                   :link_user_id => link_user_id)
+                    :link_id => link_id,
+                    :link_user_id => link_user_id)
   else
+    flash[:notice] = "You have favourited this link before!"
   end
   redirect to '/'
 end
