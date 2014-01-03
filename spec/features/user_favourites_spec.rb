e0faf66e7ddb61f4de16c92e66ac6caddae63056
Hannah Knights <hannah.knights@hotmail.co.uk>
2014-01-03 19:20:04 +0000

    Refactoring views and tests

--- spec/features/user_favourites_spec.rb
+++ spec/features/user_favourites_spec.rb
@@ -4,18 +4,16 @@ require_relative 'helpers/session'
 include SessionHelpers
 
 
-feature "Favourite links" do 
+feature "Links" do 
 
-  scenario "are created by the user" do
+  scenario "are given a default 'favourite' when they are created" do
     sign_up
     sign_in('test@test.com', '123test')
     add_link("http://makersacademy.com",
             "Makers Academy",
             ['education'])
     visit '/'
-    click_button "Add to favourites"
-    click_link "My Favourites"
-    expect(page).to have_content("Makers Academy")
+    expect(page).to have_content("1 Makers Academy")
   end
 
   scenario "can be favourited by many users" do
@@ -25,7 +23,6 @@ feature "Favourite links" do
             "Makers Academy", 
             ['education'])
     visit '/'
-    click_button "Add to favourites"
     click_button "Sign out"
     User.create(:email => 'another@another.com',
                 :password => '123test',
@@ -35,5 +32,21 @@ feature "Favourite links" do
     expect(page).to have_content("2 Makers Academy education")
   end
 
+  scenario "appear in a users 'my favourite' when 'favourited'" do
+    sign_up
+    sign_in('test@test.com', '123test')
+    add_link("http://makersacademy.com",
+            "Makers Academy", 
+            ['education'])
+    visit '/'
+    click_button "Sign out"
+    User.create(:email => 'another@another.com',
+                :password => '123test',
+                :password_confirmation => '123test')
+    sign_in('another@another.com', '123test')
+    click_button "Add to favourites"
+    click_link "My Favourites"
+    expect(page).to have_content("My Favourite Links 2 Makers Academy education")
+  end
 
 end
